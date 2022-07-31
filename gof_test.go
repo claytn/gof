ac582794191844c2741f037efd4cd7ad1e3ef388
Clayton Marshall <clayton.marshall12@gmail.com>
2022-07-31 11:51:38 -0500

    added tests for playback functionality

--- gof_test.go
+++ gof_test.go
@@ -1,14 +1,13 @@
 package gof
 
 import (
-	"fmt"
 	"image"
 	"testing"
 	"time"
 )
 
 func TestGofPlayerCreation(t *testing.T) {
-	_, err := New("./sample.gif", func(img *image.Image) {})
+	_, err := New("./test.gif", func(img *image.Image) {})
 
 	if err != nil {
 		t.Errorf("Failed to create GofPlayer instance")
@@ -17,15 +16,91 @@ func TestGofPlayerCreation(t *testing.T) {
 
 func TestGofPlayerRender(t *testing.T) {
 	ch := make(chan string)
-	gp, _ := New("./sample.gif", func(img *image.Image) {
-		time.Sleep(1 * time.Second)
+	gp, _ := New("./test.gif", func(img *image.Image) {
 		ch <- "rendered"
 	})
+
+	expectedRenderTimeWithPadding := gp.delay + (time.Millisecond * 10)
 
 	select {
 	case <-ch:
 		// Do nothing
-	case <-time.After(gp.delay):
-		t.Errorf(fmt.Sprintf("Expected render to be called within %s", gp.delay))
+	case <-time.After(expectedRenderTimeWithPadding):
+		t.Errorf("Expected render to be called within %s", expectedRenderTimeWithPadding)
+	}
+}
+
+/**
+ * The below tests rely on the test.gif file specifically which contains a
+ * single pixel gif that increases it's R value in the RGBA color of the pixel.
+ * This makes it easy to test if the playback controls are working as expected
+ */
+func TestGofRenderOrderPlay(t *testing.T) {
+	ch := make(chan *image.Image)
+	gp, _ := New("./test.gif", func(img *image.Image) {
+		ch <- img
+	})
+	// Redundant direction set for readability
+	gp.SetFrameDirection(PLAY)
+
+	var prev *image.Image = nil
+	frame := 0
+	frameCount := len(gp.frames)
+	for img := range ch {
+		frame++
+		if frame >= frameCount {
+			// Consider this test done after looping through the full GIF
+			break
+		}
+
+		if prev == nil {
+			prev = img
+			continue
+		}
+
+		r, _, _, _ := (*img).At(0, 0).RGBA()
+		oldR, _, _, _ := (*prev).At(0, 0).RGBA()
+		if r <= oldR {
+			t.Errorf("Expected frames to be played in an increasing order")
+		}
+
+		prev = img
+	}
+}
+
+func TestGofRenderOrderRewind(t *testing.T) {
+	ch := make(chan *image.Image)
+	gp, _ := New("./test.gif", func(img *image.Image) {
+		ch <- img
+	})
+	gp.SetFrameDirection(REWIND)
+
+	var prev *image.Image = nil
+	frame := 0
+	frameCount := len(gp.frames)
+	for img := range ch {
+		frame++
+		if frame >= frameCount {
+			// Consider this test done after looping through the full GIF
+			break
+		}
+
+		if prev == nil {
+			prev = img
+			continue
+		}
+
+		r, _, _, _ := (*img).At(0, 0).RGBA()
+		oldR, _, _, _ := (*prev).At(0, 0).RGBA()
+		/**
+		 * Only enforce this once passed the first two frames
+		 * On reverse, the first frame is still the 0th frame so going from 0th to (len - 1)
+		 * will result in this test failing.
+		 */
+		if r >= oldR && frame > 2 { // The first frame is the 0th frame so going in reverse ...
+			t.Errorf("Expected frames to be played in an decreasing order")
+		}
+
+		prev = img
 	}
 }
